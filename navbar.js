1b5ffc1364b811611c1341cdb5419d3153fd194e
AXV.26 <ahmed.walled.saad@gmail.com>
2026-03-08 19:39:47 +0400

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -2,7 +2,7 @@
     const navbar = document.createElement('nav');
     navbar.className = 'navbar';
     navbar.innerHTML = `
-        <a href="./index.html" class="navbar-brand">
+        <a href="/index.html" class="navbar-brand">
             <img src="/logo2.png" alt="IFelx Logo">
             <span class="navbar-brand-name">IFelx Web</span>
         </a>
@@ -16,13 +16,15 @@
             " />
         </div>
         <div class="navbar-links">
-            <a href="./jowa-gpt/index.html">Jowa-GPT</a>
-            <a href="./jowa-football-ai/index.html">Football AI</a>
-            <a href="./ifelx/index.html">IFelxOS</a>
+            <a href="/jowa-gpt/index.html">Jowa-GPT</a>
+            <a href="/jowa-football-ai/index.html">Football AI</a>
+            <a href="/ifelx/index.html">IFelxOS</a>
             <a href="https://wex-br.github.io">WEX</a>
+            <a href="https://www.instagram.com/axv.bin" target="_blank" class="navbar-instagram">
+                <img src="https://static.cdninstagram.com/rsrc.php/v4/yI/r/VsNE-OHk_8a.png" alt="Instagram" class="navbar-insta-icon">
+                axv.bin
+            </a>
         </div>
     `;
     document.body.insertBefore(navbar, document.body.firstChild);
-
 })();
-
